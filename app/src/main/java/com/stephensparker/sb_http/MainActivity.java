8dffa93723e9bf52b62f564f0360845352e7bd4d
ssp2306 <stejodi@gmail.com>
2015-02-25 16:12:49 -0700

    updated

--- app/src/main/java/com/stephensparker/sb_http/MainActivity.java
+++ app/src/main/java/com/stephensparker/sb_http/MainActivity.java
@@ -1,5 +1,13 @@
 package com.stephensparker.sb_http;
 
+
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import android.app.Activity;
 import android.os.*;
 import android.view.Menu;
@@ -8,16 +16,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.io.*;
-import java.net.URL;
-import java.net.HttpURLConnection;
 
+import android.os.Bundle;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
     private Button buttonOpenURL;
-    String response;
-    TextView tOutputURL;
+    TextView txtViewParsedValue;
+    private JSONObject jsonObject;
+
+    String str = "";
+    String strParsedValue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,7 @@ public class MainActivity extends Activity {
         StrictMode.setThreadPolicy(policy);
 
         // find the ID of the TextView
-        tOutputURL = (TextView) findViewById(R.id.editTextURLOutput);
+        txtViewParsedValue = (TextView) findViewById(R.id.editTextURLOutput);
         buttonOpenURL = (Button) findViewById(R.id.buttonOpenURL);
 
         addListenerOnClick();
@@ -45,48 +57,83 @@ public class MainActivity extends Activity {
                 // grab values from the input fields and pass them to the output text
                 EditText editURL = (EditText) findViewById(R.id.editURL);
                 String eURL = editURL.getText().toString();
-
+                HttpClient myClient = new DefaultHttpClient();
+                HttpPost myConnection = new HttpPost(eURL);
+                HttpResponse response;
                 try {
-                    URL url = new URL(eURL);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    readStream(con.getInputStream());
 
-                } catch (Exception e) {
+                    response = myClient.execute(myConnection);
+                    str = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+                } catch (ClientProtocolException e) {
                     e.printStackTrace();
-                    response = "Something Wrong " + e.toString();
-                    tOutputURL.setText(response);
-
-                }
-
-            }
-
-            private void readStream(InputStream in) {
-                BufferedReader reader = null;
-                try {
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-                        //System.out.println(line);
-                        response += line;
-
-
-                    }
-                    tOutputURL.setText(response);
+                    txtViewParsedValue.setText(e.toString());
                 } catch (IOException e) {
                     e.printStackTrace();
-                    response = "Something Wrong " + e.toString();
-                    tOutputURL.setText(response);
-                } finally {
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            response = "Something Wrong " + e.toString();
-                            tOutputURL.setText(response);
-                        }
-                    }
+                    txtViewParsedValue.setText(e.toString());
                 }
+
+
+                try{
+
+                    jsonObject = new JSONObject(str);
+
+                    JSONObject objectFamily = jsonObject.getJSONObject("FamilyName");
+
+                    // get the family name from the JSON object
+                    strParsedValue = "Meet the " + objectFamily.getString("Family") + " clan:\n";
+
+                    /// create a sub array from the JSON string
+                    JSONObject subObject = objectFamily.getJSONObject("sub");
+                    JSONArray subArray = subObject.getJSONArray("subdata");
+
+                    strParsedValue += "There are " + subArray.length() + " people in the family.\n\n";
+
+                    for (int i = 0; i < subArray.length(); i++) {
+                        // get the info on each family member
+                        strParsedValue += subArray.getJSONObject(i).getString("name");
+                        strParsedValue +=" is " + subArray.getJSONObject(i).getString("age");
+                        strParsedValue += subArray.getJSONObject(i).getString("gender");
+                        strParsedValue += " loves " + subArray.getJSONObject(i).getString("favcolor") + ".\n";
+                    }
+                    txtViewParsedValue.setText(strParsedValue);
+                } catch ( JSONException e) {
+                    e.printStackTrace();
+                    txtViewParsedValue.setText(e.toString());
+                }
+
+//
+//            private void readStream(InputStream in) {
+//                BufferedReader reader = null;
+//                try {
+//
+//                    reader = new BufferedReader(new InputStreamReader(in));
+//                    String line = "";
+//                    while ((line = reader.readLine()) != null) {
+//                        //System.out.println(line);
+//                        response += line;
+//
+//
+//                    }
+//                    //txtViewParsedValue.setText(response);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    response = "Something Wrong " + e.toString();
+//                    txtViewParsedValue.setText(response);
+//                } finally {
+//                    if (reader != null) {
+//                        try {
+//                            reader.close();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                            response = "Something Wrong " + e.toString();
+//                            txtViewParsedValue.setText(response);
+//                        }
+//                    }
+//                }
+//            }
+
+
             }
 
         });
@@ -115,3 +162,5 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
